bbc28ff16038c6434766aff67ff95bdf5f6c9642
Lucas Garcia Nachtigall <69644385+lucas-nachtigall@users.noreply.github.com>
2023-07-28 14:27:22 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,1 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  compress: false,
-  images: {
-    unoptimized: false
-  }
-}
+
